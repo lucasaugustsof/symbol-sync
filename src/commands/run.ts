5f7bc3b829aba19d9951017100657cc0bd49507d
Lucas Augusto <lucasaugustscode@gmail.com>
2024-01-28 17:09:42 -0300

    feat: implementing commander

---
+++ src/commands/run.ts
@@ -0,0 +1,127 @@
+import { Command } from 'commander'
+
+import chalk from 'chalk'
+
+import { FigmaService } from '~/services/figma-service'
+
+import { APP_NAME } from '~/utils/const'
+import { createComponent } from '~/utils/create-component'
+import { getConfig } from '~/utils/get-config'
+import { getMappedIcons } from '~/utils/get-mapped-icons'
+import { libBuilder } from '~/utils/lib-builder'
+import { logger } from '~/utils/logger'
+
+type CommandOpts = {
+  build: boolean
+}
+
+type CategoryData = {
+  id: string
+  name: string
+  code?: string
+}
+
+const figmaService = new FigmaService()
+
+class RunCommand extends Command {
+  constructor() {
+    super()
+
+    this.name('run')
+    this.description('Command to sync icons')
+
+    this.option('--build', 'option to create React icon library')
+
+    this.action((opts: CommandOpts) => this.execute(opts))
+  }
+
+  async execute(opts: CommandOpts) {
+    const { fileId, categories } = await getConfig()
+
+    const figmaDocument = await figmaService.retrieveCloudDocumentData(fileId)
+
+    const symbolSyncPage = figmaDocument.children.find(
+      (page) => page.name === APP_NAME,
+    )
+
+    if (!symbolSyncPage) {
+      logger.error(
+        `
+        Error: The expected ${chalk.white(
+          APP_NAME,
+        )} page was not found in the Figma document.
+        Please check if the page name is correct and if it exists in the current document.
+        `,
+      )
+
+      return
+    }
+
+    const categoriesWithIcons = symbolSyncPage.children
+      .filter((category) => {
+        const categoryName = category.name.toLowerCase()
+
+        if (categories.includes(categoryName)) {
+          return category
+        }
+
+        return null
+      })
+      .filter(Boolean)
+
+    const categoryData: Record<string, CategoryData[]> = {}
+
+    for (const categoryFound of categoriesWithIcons) {
+      const categoryName = categoryFound.name.toLowerCase()
+      categoryData[categoryName] = []
+
+      categoryFound.children.forEach(({ id, name }) => {
+        categoryData[categoryName].push({
+          id,
+          name,
+        })
+      })
+    }
+
+    // TODO - Refactor code for better modularity by breaking down large functions into smaller, clearer units
+
+    for (const category in categoryData) {
+      const categoryIconIdentifiers = categoryData[category].map(({ id }) => id)
+
+      const iconUrlMappings = await figmaService.retrieveCloudImageInfo(
+        categoryIconIdentifiers,
+      )
+
+      const mappedIcons = await getMappedIcons(iconUrlMappings)
+
+      categoryData[category] = categoryData[category].map(({ id, name }) => {
+        return {
+          id,
+          name,
+          code: mappedIcons[id],
+        }
+      })
+    }
+
+    // TODO - Leave this variable in the config file
+
+    const entry = './src/components'
+
+    // TODO - Refactor this function after CLI implementation
+
+    for await (const icon of categoryData['attention']) {
+      await createComponent(entry, {
+        name: icon.name,
+        code: icon.code!,
+      })
+    }
+
+    if (opts.build) {
+      await libBuilder(entry, {
+        outDir: 'lib',
+      })
+    }
+  }
+}
+
+export const runCommand = new RunCommand()
